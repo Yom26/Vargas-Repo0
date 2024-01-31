26f82b44217c147b5be34963bc612fb898cf2b63
Vargas, Joel <157347875+Yom26@users.noreply.github.com>
2024-01-31 19:25:11 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    
+    var userName = prompt("Please enter your name:");
+
+    
+    alert("Welcome to User/Student: " + userName);
+});
+
+function printStudentList() {
+    window.print();
+}
